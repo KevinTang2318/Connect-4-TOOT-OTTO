092a16b3cacfd24e33890219ca73a230f65d5533
Kevin Tang <kaiwen2@ualberta.ca>
2022-03-24 22:24:56 -0600

    Add routes for post and get request for /game

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,14 +2,21 @@
 #[macro_use] extern crate rocket;
 
 use rocket::response::NamedFile;
+use rocket::State;
 use std::path::{Path, PathBuf};
+use bson::{bson, Bson};
+use rocket_contrib::json::Json;
+use serde::{Serialize, Deserialize};
 
 use mongodb::{
     bson::doc,
     sync::Client,
+    sync::Collection,
 };
 
 // Collection Schema
+#[derive(Serialize, Deserialize, Debug)]
+#[allow(non_snake_case)]
 pub struct Game {
     GameDate: i64,
     gameType: String,
@@ -19,10 +26,38 @@ pub struct Game {
     WinnerName: String,
 }
 
+//--------------------------------------------------------------------------------
+
 // The is the way we route to certain HTML page
 #[get("/")]
 fn index() -> Option<NamedFile> {
     NamedFile::open(Path::new("target/deploy/index.html")).ok()
+}
+
+#[get("/games")]
+fn get(collection: State<Collection<Game>>) -> Json<Vec<Game>> {
+    let mut games = Vec::new();
+    
+    // get all the documents in the collection
+    if let Ok(mut cursor) = collection.find(doc!{}, None) {
+
+        while let Some(game) = cursor.next() {
+            if let Ok(game_doc) = game {
+                games.push(game_doc);         //TODO: Need to check whether the game_doc here is parsed as Game correctly or is stll bson style
+            }
+        }
+    }
+    
+    Json(games)
+}
+
+#[post("/games", format="json", data="<game_json>")]
+fn post(game_json: Json<Game>, collection: State<Collection<Game>>) {
+
+    if let game = game_json.into_inner() {
+        collection.insert_one(game, None).unwrap();
+    }
+
 }
 
 fn rocket() -> Result<rocket::Rocket, mongodb::error::Error> { 
